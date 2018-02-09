9c8a04febbf829f01cc1c6a82a3f5a9e2fe526df
Alakazam03 <vaibhav.045@iiitb.org>
2018-02-09 14:45:45 +0530

    second

--- ch1.py
+++ ch1.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 from collections import Counter
 
+#hdkjfhdjhksdjhk
 def make_Dictionary(root_dir):
     all_words=[]
     emails=[os.path.join(root_dir,f) for f in os.listdir(root_dir)]
@@ -22,6 +23,33 @@ def make_Dictionary(root_dir):
 
     return dictionary.most_common(3000)
 
+def extract_features(mail_dir):
+    files=[os.path.join(mail_dir, fi) for fi in os.listdir(mail_dir)]
+    features_matrix = np.zeros((len(files),3000))
+    train_labels = np.zeros(len(files))
+    count=0
+    docID=0
+    for fil in files:
+        with open(fil) as fi:
+            for i,line in enumerate(fi):
+                if i==2:
+                    words = line.split()
+                    for word in words:
+                        wordID=0
+                        for i,d in enumerate(dictionary):
+                            print word
+                            if d[0]==word:
+                                  wordID = i
+                                  features_matrix[docID,wordID] = words.count(word)
+                        train_labels[docID] = 0;
+                        filepathTokens = fil.split('/')
+                        lastToken = filepathTokens[len(filepathTokens) - 1]
+                        if lastToken.startswith("spmsg"):
+                            train_labels[docID] = 1;
+                            count = count + 1
+                        docID = docID + 1
+                    return features_matrix, train_labels
+
 
 
 
@@ -33,4 +61,5 @@ TRAIN_DIR = "train-mails"
 TEST_DIR = "test-mails"
 
 dictionary = make_Dictionary(TRAIN_DIR)
+test_feature_matrix, test_labels = extract_features(TEST_DIR)
 print (dictionary)
